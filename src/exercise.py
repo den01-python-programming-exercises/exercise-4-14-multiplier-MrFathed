99e0f0159a598e16dc3156a1b4f60ed72667b279
Justin Turner <jmturner638@gmail.com>
2021-08-04 17:13:14 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
+from multiplier import Multiplier
+
 def main():
     #write your code below this line
+    multiply_by_three = Multiplier(3)
+
+    print("multiply_by_three.multiply(2): " + str(multiply_by_three.multiply(2)))
+
+    multiply_by_four = Multiplier(4)
+
+    print("multiply_by_four.multiply(2): " + str(multiply_by_four.multiply(2)))
+    print("multiply_by_three.multiply(1): " + str(multiply_by_three.multiply(1)))
+    print("multiply_by_four.multiply(1): " + str(multiply_by_four.multiply(1)))
+
 
 if __name__ == '__main__':
     main()
